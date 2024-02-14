e4a9fae700151e4d8b353fdaac96466a5920069e
anael <anael@BOREDGUY28>
2024-02-13 18:37:37 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿int dia;
+int op;
+
+Console.WriteLine("Coloca un numero: ");
+op = int.Parse(Console.ReadLine());
+
+switch (op)
+{
+    case 1:
+        Console.WriteLine("Es Lunes");
+        break;
+    case 2:
+        Console.WriteLine("Es Martes");
+        break;
+    case 3:
+        Console.WriteLine("Es Miercoles");
+        break;
+    case 4:
+        Console.WriteLine("Es Jueves");
+        break;
+    case 5:
+        Console.WriteLine("Es Viernes");
+        break;
+    case 6:
+        Console.WriteLine("Es Sabado");
+        break;
+    case 7:
+        Console.WriteLine("Es Domingo");
+        break;
+
+}
